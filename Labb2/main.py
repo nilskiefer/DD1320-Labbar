6773679922c25079376aaa151086b36fec4af6da
nils_kiefer <21311514+nilsonstine@users.noreply.github.com>
2024-01-19 17:11:17 +0100

    Uppgift 1-2

---
+++ Labb2/main.py
@@ -0,0 +1,15 @@
+from arrayQFile import ArrayQ
+
+def main():
+    q = ArrayQ()
+    q.enqueue(1)
+    q.enqueue(2)
+    x = q.dequeue()
+    y = q.dequeue()
+    if (x == 1 and y == 2):
+        print("OK")
+    else:
+        print("FAILED")
+
+if __name__ == "__main__":
+    main()
